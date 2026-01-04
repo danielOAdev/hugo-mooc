8dd090e8a4bb4771b992d58afbcf76831fbd0b33
Daniel de Oliveira <danielrtt32@outlook.com>
2026-01-04 18:08:24 -0300

    Implementa flipcards

---
+++ assets/recursos/js/vigi/flipcard.mjs
@@ -0,0 +1,42 @@
+class Flipcard {
+    constructor(elemento) {
+        if (!elemento.classList.contains("flipcard")) {
+            throw new Error('O elemento deve ter a classe "flipcard".');
+        }
+
+        this.flipcard = elemento;
+
+        this.input = this.flipcard.querySelector('& > input');
+        if (!this.input) {
+            throw new Error('Input não encontrado.');
+        }
+
+        this.botaoFrente = this.flipcard.querySelector('.flipcard-frente .flipcard-btn');
+        if (!this.botaoFrente) {
+            throw new Error('Botão do lado da frente não encontrado.');
+        }
+
+        this.botaoVerso = this.flipcard.querySelector('.flipcard-verso .flipcard-btn');
+        if (!this.botaoVerso) {
+            throw new Error('Botão do lado da verso não encontrado.');
+        }
+
+        this.botaoFrente.addEventListener('click', () => {
+            this.input.checked = !this.input.checked;
+            this.botaoVerso.focus();
+        })
+
+        this.botaoVerso.addEventListener('click', () => {
+            this.input.checked = !this.input.checked;
+            this.botaoFrente.focus();
+        })
+    }
+}
+
+document.querySelectorAll('.flipcard').forEach(elementoFlipcard => {
+    try {
+        new Flipcard(elementoFlipcard);
+    } catch (error) {
+        console.error(error);
+    }
+});
